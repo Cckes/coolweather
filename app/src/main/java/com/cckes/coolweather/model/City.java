882f3425a13e506f5d11386f7bf8cc22479ab4b1
Alvin <hqcckes@gmail.com>
2015-12-26 05:42:21 +0800

    By Alvin Add more source files Modify some files Delete files that don't need

---
+++ app/src/main/java/com/cckes/coolweather/model/City.java
@@ -0,0 +1,45 @@
+package com.cckes.coolweather.model;
+
+/**
+ * Created by AlvinYuan on 2015/12/26.
+ */
+public class City {
+
+    private int id;
+    private String cityName;
+    private String cityCode;
+    private int provinceId;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public void setCityCode(String cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+
+    public int getId() {
+
+        return id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public String getCityCode() {
+        return cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+}
---
+++ app/src/main/java/com/cckes/coolweather/util/HttpCallbackListener.java
@@ -0,0 +1,9 @@
+package com.cckes.coolweather.util;
+
+public interface HttpCallbackListener {
+
+	void onFinish(String response);
+
+	void onError(Exception e);
+
+}
